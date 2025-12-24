2578daee01247a4337926e469522328c4ee8b613
Kamran Muradov <muradkamran35@gmail.com>
2025-12-24 06:03:50 +0400

    Add files via upload

---
+++ backend/MyApp.Application/DTOs/Tests/TestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MyApp.Application.DTOs.Tests;
+
+public sealed class TestDto
+{
+    public int Id { get; init; }
+    public string Name { get; init; } = string.Empty;
+    public int CategoryId { get; init; }
+}
