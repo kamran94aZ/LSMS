fff5b1c399b19992d91693072959977597972a8d
Kamran Muradov <muradkamran35@gmail.com>
2025-12-24 05:23:52 +0400

    Add global exception filter

---
+++ backend/MyApp.Api/Filters/ExceptionFilter.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MyApp.Domain.Exceptions;
+
+namespace MyApp.API.Filters;
+
+public sealed class ExceptionFilter : IExceptionFilter
+{
+    public void OnException(ExceptionContext context)
+    {
+        if (context.Exception is ForbiddenException)
+        {
+            context.Result = new ObjectResult(new
+            {
+                code = "FORBIDDEN"
+            })
+            {
+                StatusCode = StatusCodes.Status403Forbidden
+            };
+
+            context.ExceptionHandled = true;
+            return;
+        }
+
+        if (context.Exception is NotFoundException)
+        {
+            context.Result = new NotFoundResult();
+            context.ExceptionHandled = true;
+            return;
+        }
+
+        if (context.Exception is SecurityViolationException)
+        {
+            context.Result = new ObjectResult("Bad request")
+            {
+                StatusCode = StatusCodes.Status400BadRequest
+            };
+            context.ExceptionHandled = true;
+            return;
+        }
+
+        context.Result = new ObjectResult(new
+        {
+            error = context.Exception.GetType().Name,
+            message = context.Exception.Message,
+            stack = context.Exception.StackTrace
+        })
+        {
+            StatusCode = StatusCodes.Status500InternalServerError
+        };
+
+        context.ExceptionHandled = true;
+
+    }
+}
+
