e38cdbb9a70cae4834ee15bbd3688775e2f56fbb
Kamran Muradov <muradkamran35@gmail.com>
2025-12-24 05:44:31 +0400

    Add files via upload

---
+++ backend/MyApp.Application/DTOs/Auth/LoginDto.cs
@@ -0,0 +1,9 @@
+﻿namespace MyApp.Application.DTOs.Auth;
+
+public sealed class LoginDto
+{
+    public string Username { get; init; } = string.Empty;
+    public string Password { get; init; } = string.Empty;
+}
+
+
