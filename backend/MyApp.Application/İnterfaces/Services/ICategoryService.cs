3d03daae62dcfbe192830fd9a3e6b96e21164112
Kamran Muradov <muradkamran35@gmail.com>
2025-12-24 10:42:52 +0400

    Add files via upload

---
+++ backend/MyApp.Application/İnterfaces/Services/ICategoryService.cs
@@ -0,0 +1,11 @@
+﻿using MyApp.Application.DTOs.Categories;
+
+namespace MyApp.Application.Interfaces.Services;
+
+public interface ICategoryService
+{
+    IReadOnlyList<CategoryDto> GetAll();
+    void Create(CreateCategoryDto dto);
+    void Delete(int id);
+}
+
