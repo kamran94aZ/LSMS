5eba974f0d2e9f9a9be32aba1e5b885e19a92b50
Kamran Muradov <muradkamran35@gmail.com>
2025-12-24 11:25:01 +0400

    Add files via upload

---
+++ backend/MyApp.Domain/Interfaces/Repositories/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using MyApp.Domain.Entities;
+
+namespace MyApp.Domain.Interfaces.Repositories;
+
+public interface ICategoryRepository
+{
+    IReadOnlyList<Category> GetAll();
+    Category? GetById(int id);
+    void Add(Category category);
+    void Delete(Category category);
+}
+
