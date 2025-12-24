7cf0cb18c0e5919c87b52746a458b96464dc8d8f
Kamran Muradov <muradkamran35@gmail.com>
2025-12-24 11:09:12 +0400

    Add files via upload

---
+++ backend/MyApp.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyApp.Data.Context;
+using MyApp.Domain.Entities;
+using MyApp.Domain.Interfaces.Repositories;
+using System;
+
+namespace MyApp.Data.Repositories;
+
+public sealed class CategoryRepository : ICategoryRepository
+{
+    private readonly AppDbContext _ctx;
+
+    public CategoryRepository(AppDbContext ctx)
+    {
+        _ctx = ctx;
+    }
+
+    public IReadOnlyList<Category> GetAll()
+        => _ctx.Categories.AsNoTracking().OrderBy(x => x.Id).ToList();
+
+    public Category? GetById(int id)
+        => _ctx.Categories.SingleOrDefault(x => x.Id == id);
+
+    public void Add(Category category)
+    {
+        _ctx.Categories.Add(category);
+        _ctx.SaveChanges();
+    }
+
+    public void Delete(Category category)
+    {
+        _ctx.Categories.Remove(category);
+        _ctx.SaveChanges();
+    }
+}
+
