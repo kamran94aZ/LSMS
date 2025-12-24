58cfd8b6dfadec767e5e228416942303ce662cbc
Kamran Muradov <muradkamran35@gmail.com>
2025-12-24 10:42:07 +0400

    Add files via upload

---
+++ backend/MyApp.Application/İnterfaces/Services/IAdminAuthService.cs
@@ -0,0 +1,9 @@
+﻿using MyApp.Application.DTOs.Auth;
+
+namespace MyApp.Application.Interfaces.Services;
+
+public interface IAdminAuthService
+{
+    LoginResult Login(LoginDto dto);
+    bool ChangePassword(int userId, string newPassword);
+}
