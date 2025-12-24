a820a24594a1d56ccff0c8a9044bc4d883de61b9
Kamran Muradov <muradkamran35@gmail.com>
2025-12-24 11:07:13 +0400

    Add files via upload

---
+++ backend/MyApp.Data/Repositories/AdminUserRepository.cs
@@ -0,0 +1,34 @@
+﻿using MyApp.Data.Context;
+using MyApp.Domain.Entities;
+using MyApp.Domain.Interfaces.Repositories;
+
+namespace MyApp.Data.Repositories;
+
+public sealed class AdminUserRepository : IAdminUserRepository
+{
+    private readonly AppDbContext _ctx;
+
+    public AdminUserRepository(AppDbContext ctx)
+    {
+        _ctx = ctx;
+    }
+
+    public AdminUser? GetByUserName(string adminUserName)
+    {
+        return _ctx.AdminUsers
+            .SingleOrDefault(x => x.AdminUserName == adminUserName);
+    }
+
+    public AdminUser? GetById(int id)
+    {
+        return _ctx.AdminUsers
+            .SingleOrDefault(x => x.Id == id);
+    }
+
+    public void Update(AdminUser user)
+    {
+        _ctx.AdminUsers.Update(user);
+        _ctx.SaveChanges();
+    }
+}
+
