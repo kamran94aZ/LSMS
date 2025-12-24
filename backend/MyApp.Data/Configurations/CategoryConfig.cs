3eba5424828ab414f538a6d1eb8a7fdc8462ad95
Kamran Muradov <muradkamran35@gmail.com>
2025-12-24 10:58:58 +0400

    Add files via upload

---
+++ backend/MyApp.Data/Configurations/CategoryConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyApp.Domain.Entities;
+
+namespace MyApp.Data.Configurations;
+
+public sealed class CategoryConfig : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.ToTable("Categories");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(x => x.IsLocked)
+            .IsRequired();
+    }
+}
