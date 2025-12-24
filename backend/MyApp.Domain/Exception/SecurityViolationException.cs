cf7d73615fadcee04d292994725eb3f37473bbfb
Kamran Muradov <muradkamran35@gmail.com>
2025-12-24 11:21:51 +0400

    Add files via upload

---
+++ backend/MyApp.Domain/Exception/SecurityViolationException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyApp.Domain.Exceptions
+{
+    public sealed class SecurityViolationException : Exception
+    {
+        public SecurityViolationException(string message)
+            : base(message)
+        {
+        }
+    }
+}
+
