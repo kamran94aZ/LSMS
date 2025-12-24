5f18f0b0ebbeecb9ab21463476ac74724e616113
Kamran Muradov <muradkamran35@gmail.com>
2025-12-24 11:09:46 +0400

    Add files via upload

---
+++ backend/MyApp.Data/Repositories/TestRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyApp.Data.Context;
+using MyApp.Domain.Entities;
+using MyApp.Domain.Interfaces.Repositories;
+
+namespace MyApp.Data.Repositories;
+
+public sealed class TestRepository : ITestRepository
+{
+    private readonly AppDbContext _ctx;
+    public TestRepository(AppDbContext ctx) => _ctx = ctx;
+
+    public Test? GetById(int id)
+        => _ctx.Tests.AsNoTracking().SingleOrDefault(x => x.Id == id);
+
+    public IReadOnlyList<Test> GetAll()
+        => _ctx.Tests.AsNoTracking().OrderBy(x => x.Id).ToList();
+
+    public void Add(Test test)
+    {
+        _ctx.Tests.Add(test);
+        _ctx.SaveChanges();
+    }
+
+
+    public void Delete(Test test)
+    {
+        _ctx.Tests.Remove(test);
+        _ctx.SaveChanges();
+    }
+}
+
