f6ff5b3d708319e2b6a82f0709fae4ef166cad92
Kamran Muradov <muradkamran35@gmail.com>
2025-12-24 05:46:12 +0400

    Add files via upload

---
+++ backend/MyApp.Application/DTOs/Auth/LoginResult.cs
@@ -0,0 +1,14 @@
+﻿using MyApp.Domain.Enums;
+
+namespace MyApp.Application.DTOs.Auth;
+
+public sealed class LoginResult
+{
+    public bool Success { get; set; }
+    public int UserId { get; set; }
+    public UserRole Role { get; set; }
+
+    public bool MustChangePassword { get; set; }
+}
+
+
