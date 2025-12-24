44e5ec18f7c94c114842ce24e8ff9cce0eba1d4d
Kamran Muradov <muradkamran35@gmail.com>
2025-12-24 10:43:52 +0400

    Add files via upload

---
+++ backend/MyApp.Application/İnterfaces/Services/ITestService.cs
@@ -0,0 +1,11 @@
+﻿using MyApp.Application.DTOs.Tests;
+
+namespace MyApp.Application.Interfaces.Services;
+
+public interface ITestService
+{
+    IReadOnlyList<TestDto> GetAll();
+    void Create(CreateTestDto dto);
+    void Delete(int testId);
+}
+
