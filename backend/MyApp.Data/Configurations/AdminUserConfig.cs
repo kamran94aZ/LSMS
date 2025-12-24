2c9538eb7da43861b869a711187ee22ab56621c8
Kamran Muradov <muradkamran35@gmail.com>
2025-12-24 10:57:19 +0400

    Add files via upload

---
+++ backend/MyApp.Data/Configurations/AdminUserConfig.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyApp.Domain.Entities;
+
+namespace MyApp.Data.Configurations;
+
+public sealed class AdminUserConfig : IEntityTypeConfiguration<AdminUser>
+{
+    public void Configure(EntityTypeBuilder<AdminUser> builder)
+    {
+        builder.ToTable("AdminUsers");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id).HasColumnName("AdminUserID");
+        builder.Property(x => x.AdminUserName).HasColumnName("AdminUserName").IsRequired().HasMaxLength(100);
+        builder.Property(x => x.FullName).HasMaxLength(200);
+        builder.Property(x => x.JobTitle).HasMaxLength(100);
+        builder.Property(x => x.IsActive).IsRequired();
+        builder.Property(x => x.PasswordHash).IsRequired().HasMaxLength(500);
+
+        builder.Property(x => x.CreatedAt);
+        builder.Property(x => x.UpdatedAt);
+        builder.Property(x => x.LastLoginAt);
+
+        builder.HasIndex(x => x.AdminUserName).IsUnique();
+    }
+}
+
