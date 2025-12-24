fa11c78ae95e8a6774f26ff5dace4cde11928c6f
Kamran Muradov <muradkamran35@gmail.com>
2025-12-24 06:07:05 +0400

    Add files via upload

---
+++ backend/MyApp.Application/İnterfaces/Security/ICurrentUser.cs
@@ -0,0 +1,15 @@
+﻿using MyApp.Domain.Enums;
+
+namespace MyApp.Application.Interfaces.Security;
+
+public interface ICurrentUser
+{
+    int UserId { get; }
+    UserRole Role { get; }
+    bool IsAuthenticated { get; }
+    bool MustChangePassword { get; }
+
+    bool IsAdmin();
+}
+
+
