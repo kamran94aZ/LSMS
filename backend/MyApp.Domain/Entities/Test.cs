8c32958e354dc4b04737062437bf959e93350843
Kamran Muradov <muradkamran35@gmail.com>
2025-12-24 11:15:24 +0400

    Add files via upload

---
+++ backend/MyApp.Domain/Entities/Test.cs
@@ -0,0 +1,25 @@
+﻿using MyApp.Domain.Exceptions;
+
+namespace MyApp.Domain.Entities;
+
+public class Test
+{
+    public int Id { get; private set; }
+    public string Name { get; private set; } = string.Empty;
+    public int CategoryId { get; private set; }
+    public bool IsLocked { get; private set; }
+
+    private Test() { } 
+
+    public Test(string name, int categoryId)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new SecurityViolationException("Invalid name");
+
+        Name = name.Trim();
+        CategoryId = categoryId;
+        IsLocked = false;
+    }
+
+    public void Lock() => IsLocked = true;
+}
