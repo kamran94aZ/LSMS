6d563510d6c8fbdd3e81c0e20a436aa96540c27b
Kamran Muradov <muradkamran35@gmail.com>
2025-12-24 11:31:57 +0400

    Add files via upload

---
+++ backend/MyApp.Domain/Interfaces/Security/ISecurityContext.cs
@@ -0,0 +1,11 @@
+﻿using MyApp.Domain.Enums;
+
+namespace MyApp.Domain.Interfaces.Security;
+
+public interface ISecurityContext
+{
+    int UserId { get; }
+    UserRole Role { get; }
+    bool IsAuthenticated { get; }
+}
+
