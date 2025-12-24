09550704666af421482268fea3c1865abf1d66b8
Kamran Muradov <muradkamran35@gmail.com>
2025-12-24 11:26:16 +0400

    Add files via upload

---
+++ backend/MyApp.Domain/Interfaces/Repositories/ITestRepository.cs
@@ -0,0 +1,11 @@
+﻿using MyApp.Domain.Entities;
+
+namespace MyApp.Domain.Interfaces.Repositories;
+
+public interface ITestRepository
+{
+    Test? GetById(int id);
+    IReadOnlyList<Test> GetAll();
+    void Add(Test test);
+    void Delete(Test test);
+}
