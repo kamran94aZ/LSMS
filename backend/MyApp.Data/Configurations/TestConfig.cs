ab0825f8728cc424259062f202398733724579ce
Kamran Muradov <muradkamran35@gmail.com>
2025-12-24 11:01:01 +0400

    Add files via upload

---
+++ backend/MyApp.Data/Configurations/TestConfig.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyApp.Domain.Entities;
+
+namespace MyApp.Data.Configurations;
+
+public sealed class TestConfig : IEntityTypeConfiguration<Test>
+{
+    public void Configure(EntityTypeBuilder<Test> builder)
+    {
+        builder.ToTable("Tests");
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Name).IsRequired().HasMaxLength(200);
+        builder.Property(x => x.CategoryId).IsRequired();
+        builder.Property(x => x.IsLocked).IsRequired();
+    }
+}
+
