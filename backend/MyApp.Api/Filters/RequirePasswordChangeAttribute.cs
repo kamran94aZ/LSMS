8f09dc7fede7e1208b32c4b8b3f33f370f37127f
Kamran Muradov <muradkamran35@gmail.com>
2025-12-24 05:26:37 +0400

    Add global password change attribute

---
+++ backend/MyApp.Api/Filters/RequirePasswordChangeAttribute.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MyApp.Application.Interfaces.Security;
+
+namespace MyApp.API.Filters;
+
+public sealed class RequirePasswordChangeAttribute : ActionFilterAttribute
+{
+    public override void OnActionExecuting(ActionExecutingContext context)
+    {
+        var currentUser = context.HttpContext.RequestServices
+            .GetService<ICurrentUser>();
+
+        if (currentUser == null)
+            return;
+
+        if (currentUser.IsAuthenticated && currentUser.MustChangePassword)
+        {
+            context.Result = new ObjectResult(new
+            {
+                code = "PASSWORD_CHANGE_REQUIRED"
+            })
+            {
+                StatusCode = 403
+            };
+        }
+    }
+}
+
+
