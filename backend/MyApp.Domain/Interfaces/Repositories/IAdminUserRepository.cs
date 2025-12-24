d673d1c0c8b9b6815c03377d0e2249d065abc894
Kamran Muradov <muradkamran35@gmail.com>
2025-12-24 11:23:59 +0400

    Add files via upload

---
+++ backend/MyApp.Domain/Interfaces/Repositories/IAdminUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using MyApp.Domain.Entities;
+
+namespace MyApp.Domain.Interfaces.Repositories;
+
+public interface IAdminUserRepository
+{
+    AdminUser? GetByUserName(string adminUserName);
+    AdminUser? GetById(int id);
+    void Update(AdminUser user);
+}
